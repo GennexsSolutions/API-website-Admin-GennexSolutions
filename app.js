c037c740315d281d88f9ee7f931ac471d2f3dd87
Xaiy_IT <109733313+Khampasert9328@users.noreply.github.com>
2024-06-18 13:29:55 +0700

    first app

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/image')));
-app.use('/image', express.static('image'));
+app.use('/image', express.static('/image'));
 
 
 
